a40bf195fba73de7846a70ed4f55ff5e2494878f
migo <migo.vandingo@gmail.com>
2026-02-04 19:30:23 -0500

    auth middleware

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 from platform_common.logging.logging import get_logger
 from platform_common.middleware.request_id_middleware import RequestIDMiddleware
+from platform_common.middleware.auth_middleware import AuthMiddleware
 from platform_common.exception_handling.handlers import add_exception_handlers
 from app.api.controller.health_check import router as health_router
 from app.services.pubsub_worker import start_subscriber, stop_subscriber
@@ -11,6 +12,7 @@ logger = get_logger("video.main")
 
 app = FastAPI(title="ed-video-processing-service")
 app.add_middleware(RequestIDMiddleware)
+app.add_middleware(AuthMiddleware)
 add_exception_handlers(app)
 
 
